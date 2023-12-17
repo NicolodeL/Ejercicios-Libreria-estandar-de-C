9bf4821dd4813579ef1a3bdeff09671fad2594c4
NlodeL <nlodelop@myuax.com>
2023-12-17 13:30:10 +0100

    duplico el codigo y lo cambio para tener tambien un ejemplo con numeros negativos

--- ejercicio 1/main.cpp
+++ ejercicio 1/main.cpp
@@ -5,10 +5,16 @@
 
 int main() {
     int num = 10;
-    
+
     int abs_num = abs(num);
 
     printf("El valor absoluto de %d es %d\n", num, abs_num);
+
+    int num2 = -5;
+
+    int abs_num2 = abs(num2);
+
+    printf("El valor absoluto de %d es %d\n", num2, abs_num2);
 
     return 0;
 }
