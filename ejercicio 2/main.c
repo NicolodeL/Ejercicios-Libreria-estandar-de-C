72317b8f704169a456587e2c8e8d4f553ccb4804
NlodeL <nlodelop@myuax.com>
2023-12-17 19:42:11 +0100

    añado otro ejemplo, esta vez el codigo no es capaz de diferenciar las cadenas

--- ejercicio 2/main.c
+++ ejercicio 2/main.c
@@ -13,6 +13,16 @@ int main() {
         printf("Las cadenas son diferentes.\n");
     }
 
+    char str3[] = "Igual";
+    char str4[] = "Igual";
+
+    int comparison2 = strcmp(str3, str4);
+
+    if (comparison == 0) {
+        printf("Las cadenas son iguales.\n");
+    } else {
+        printf("Las cadenas son diferentes.\n");
+    }
 
     return 0;
 }
