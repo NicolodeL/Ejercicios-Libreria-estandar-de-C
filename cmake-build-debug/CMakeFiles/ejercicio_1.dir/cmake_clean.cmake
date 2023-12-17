c0b8cb678de07c49a0ba0d837f596dac71a7b696
NlodeL <nlodelop@myuax.com>
2023-12-17 13:09:35 +0100

    configuro el cmake list y creo el primer directorio

---
+++ cmake-build-debug/CMakeFiles/ejercicio_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejercicio_1.dir/ejercicio_1/main.cpp.obj"
+  "CMakeFiles/ejercicio_1.dir/ejercicio_1/main.cpp.obj.d"
+  "ejercicio_1.exe"
+  "ejercicio_1.exe.manifest"
+  "ejercicio_1.pdb"
+  "libejercicio_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejercicio_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ejercicio_1.dir/ejercicio_1/main.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/ejercicio_1.dir/ejercicio_1/main.cpp.obj: \
+ C:\Users\nicol\OneDrive\Documents\GitHub\Ejercicios-Libreria-estandar-de-C\ejercicio\ 1\main.cpp
