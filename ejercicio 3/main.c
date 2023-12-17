037a1bd8f1e9db5fd74e89903ce55946f4d763a7
NlodeL <nlodelop@myuax.com>
2023-12-17 20:39:47 +0100

    añado el codigo que abrirá el archivo, por ahora da error ya que no tiene archivo para abrir

--- ejercicio 3/main.c
+++ ejercicio 3/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
-
+    FILE *file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir.\n");
+        return 1;
+    }
 
     return 0;
 }
