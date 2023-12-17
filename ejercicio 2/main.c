4dc90f22160eb4602440f370096cef91074ca162
NlodeL <nlodelop@myuax.com>
2023-12-17 19:39:42 +0100

    añado el codigo proporcionado para comparar 2 cadenas

--- ejercicio 2/main.c
+++ ejercicio 2/main.c
@@ -2,5 +2,17 @@
 #include <stdio.h>
 
 int main() {
+    char str1[] = "Hola";
+    char str2[] = "Mundo";
+
+    int comparison = strcmp(str1, str2);
+
+    if (comparison == 0) {
+        printf("Las cadenas son iguales.\n");
+    } else {
+        printf("Las cadenas son diferentes.\n");
+    }
+
+
     return 0;
 }
