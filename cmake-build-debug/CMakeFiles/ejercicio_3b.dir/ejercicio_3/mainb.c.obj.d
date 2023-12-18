0ac72d63256dc436996c0adcdb200bf425d2db4f
NlodeL <nlodelop@myuax.com>
2023-12-18 13:00:11 +0100

    añado para que se escriba

---
+++ cmake-build-debug/CMakeFiles/ejercicio_3b.dir/ejercicio_3/mainb.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/ejercicio_3b.dir/ejercicio_3/mainb.c.obj: \
+ C:\Users\nicol\OneDrive\Documents\GitHub\Ejercicios-Libreria-estandar-de-C\ejercicio\ 3\mainb.c \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/nicol/Clase/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
--- ejercicio 3/mainb.c
+++ ejercicio 3/mainb.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 
 int main() {
 
-    FILE *file = fopen("file.txt", "w");
+    FILE *file = fopen("C:\\Users\\nicol\\OneDrive\\Documents\\GitHub\\Ejercicios-Libreria-estandar-de-C\\ejercicio 3\\Leeme c.txt", "w");
     if (file == NULL) {
         printf("El archivo no se pudo abrir para escritura.\n");
         return 1;
     }
-
+    fprintf(file, "Hola, mundo!\n");
 
 
     return 0;
