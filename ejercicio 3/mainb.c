40b522d273bfa0d217fd3835424ebf92be9ec62b
NlodeL <nlodelop@myuax.com>
2023-12-18 13:01:27 +0100

    añado el codigo para que lea el contenido

--- ejercicio 3/mainb.c
+++ ejercicio 3/mainb.c
@@ -9,6 +9,23 @@ int main() {
     }
     fprintf(file, "Hola, mundo!\n");
 
+    fclose(file);
+
+    file = fopen("C:\\Users\\nicol\\OneDrive\\Documents\\GitHub\\Ejercicios-Libreria-estandar-de-C\\ejercicio 3\\Leeme c.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
+
+    char buffer[50];
+    fscanf(file, "%s", buffer);
+
+    // Muestra el contenido leído
+    printf("Leído del archivo: %s\n", buffer);
+
+    // Cierra el archivo después de leer
+    fclose(file);
+
 
     return 0;
 }
